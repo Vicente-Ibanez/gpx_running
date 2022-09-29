d22bcb31b2d3d8b5785afdf0b90819772023c8a0
Vinny <Vinny@Vinnys-MBP.homenet.telecomitalia.it>
2022-09-29 09:58:22 +0200

    Added new running data and preparing for additional st features

--- map_startup.py
+++ map_startup.py
@@ -23,7 +23,7 @@ import pyproj
 from quick_startup import quick_startup
 
 
-def map_create():
+def map_create(map_type):
     # Get the data from the data file and transform it into the correct format
     data = quick_startup()
 
@@ -50,23 +50,29 @@ def map_create():
     # read in the geojson file
     s2 = gpd.read_file('data/streamlit_data/myfile.geojson', driver='GeoJSON')
 
-    # use only certain column from the data file
-    gdf2 = gdf[["speed", "id", "geometry"]].copy()
+    
+        # use only certain column from the data file
+    gdf2 = gdf[[map_type, "id", "geometry"]].copy()
 
     # Merge the two dfs (df and geojson)
     gdf2 = gpd.sjoin(s2, gdf2, how='inner', op='within')
 
     # get min and max of geojsons, cutting off obsurities/annolimies
-    min, max = gdf2['speed'].quantile([0.1,0.9]).apply(lambda x: round(x, 2))
+    min, max = gdf2[map_type].quantile([0.1,0.9]).apply(lambda x: round(x, 2))
 
     # Set color map based on distribution of speed
     colormap = branca.colormap.LinearColormap(
         colors=['black', '#ecca00','#ec9b00','#ec5300','#ec2400', '#ec0000'],
-        index=gdf2['speed'].quantile([.01, 0.2,0.4,0.6,0.8]),
+        index=gdf2[map_type].quantile([.01, 0.2,0.4,0.6,0.8]),
         vmin=min,
         vmax=max
     )
-    colormap.caption="Speed Mi/Hr"
+
+    # Assign caption type
+    if map_type == "speed":
+        colormap.caption="Speed Mi/Hr"
+    else:
+        colormap.caption="Steps/Second"
 
     # Create map with initial location
     map1 = folium.Map(location=[43.7696, 11.2558], zoom_start=12)
@@ -76,7 +82,7 @@ def map_create():
                             name='Track',
                             style_function=lambda x: {                    
                                 'color': colormap(x['properties']
-                                ['speed']),                             
+                                [map_type]),                             
                                 'weight':3, 'fillOpacity':0.5
                                 }
                             ).add_to(map1)
@@ -87,6 +93,10 @@ def map_create():
     # And the Color Map legend
     colormap.add_to(map1)
 
-    # st_data = st_folium(map1, width=500, height=500)
-    # save map to html file
-    map1.save('data/streamlit_data/index.html')
+    # Save with descriptive name
+    if map_type == "speed":
+        # save map to html file
+        map1.save('data/streamlit_data/index_speed.html')
+    else:
+        # save map to html file
+        map1.save('data/streamlit_data/index_cadence.html')
--- running_st.py
+++ running_st.py
@@ -1,26 +1,49 @@
+from lib2to3.pytree import generate_matches
 import streamlit as st
 from streamlit_folium import st_folium
 import streamlit.components.v1 as components
 
 from map_startup import map_create
 
+st.title("Florence 2022")
+st.write("By Vicente Ibanez")
 
-try:
-    # Opening the html file
-    HTMLFile = open("data/streamlit_data/index.html", "r")
-    
-    # Reading the file
-    index = HTMLFile.read()
+def generate_map(map_type):
 
-    # Display the file to streamlit
-    components.html(html = index, height=800, width=800)
-except:
-    map_create()
-    # Opening the html file
-    HTMLFile = open("data/streamlit_data/index.html", "r")
-    
-    # Reading the file
-    index = HTMLFile.read()
+    try:
+        # Select the file to open
+        path = "data/streamlit_data/index_" + map_type + ".html"
+        
+        HTMLFile = open(path, "r")
 
-    # Display the file to streamlit
-    components.html(html = index, height=800, width=800) 
+        # Reading the file
+        index = HTMLFile.read()
+
+        # Display the file to streamlit
+        components.html(html = index, height=800, width=800)
+
+    except:
+
+        map_create(map_type)
+        
+        # Select the file to open
+        path = "data/streamlit_data/index_" + map_type + ".html"
+        
+        # Opening the html file
+        HTMLFile = open(path, "r")
+        
+        # Reading the file
+        index = HTMLFile.read()
+
+        # Display the file to streamlit
+        components.html(html = index, height=800, width=800) 
+
+generate_map("speed")
+
+# map_type_option = st.sidebar.selectbox(
+#     "Select Category:",
+#     ("Speed", "Cadence")#,
+#     # on_change= map_startup()
+# )
+
+# button_go = st.sidebar.button(label="GO",on_click=generate_map(map_type_option.lower()))
