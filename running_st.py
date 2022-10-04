39c27ebbf68bb296d26dd44e3bbefc61b972be46
Vinny <Vinny@Vinnys-MBP.homenet.telecomitalia.it>
2022-10-04 09:07:24 +0200

    Added data 10-3

--- running_st.py
+++ running_st.py
@@ -8,48 +8,6 @@ from map_startup import map_create
 st.title("Florence 2022")
 st.write("By Vicente Ibanez")
 
-# def generate_map(map_type):
-    
-    # try:
-    #     # Select the file to open
-    #     path = "data/streamlit_data/index_" + map_type + ".html"
-        
-    #     HTMLFile = open(path, "r")
-
-    #     # Reading the file
-    #     index = HTMLFile.read()
-
-    #     # Display the file to streamlit
-    #     components.html(html = index, height=800, width=800)
-
-    # except:
-
-    #     map_create(map_type)
-        
-    #     # Select the file to open
-    #     path = "data/streamlit_data/index_" + map_type + ".html"
-        
-    #     # Opening the html file
-    #     HTMLFile = open(path, "r")
-        
-    #     # Reading the file
-    #     index = HTMLFile.read()
-
-    #     # Display the file to streamlit
-    #     components.html(html = index, height=800, width=800) 
-
-    # return comp
-
-# prev_gen_map = generate_map("speed")
-
-# map_type_option = st.sidebar.selectbox(
-#     "Select Category:",
-#     ("Speed", "Cadence")#,
-    # on_change= map_startup()
-# )
-
-# button_go = st.sidebar.button(label="GO",on_click=generate_map(map_type_option.lower()))
-
 map_type = st.sidebar.selectbox(
     "Select Category:",
     ("speed", "cadence")
@@ -90,17 +48,5 @@ if map_type == "speed":
     generate_map(map_type)
 else:
     generate_map(map_type)
-# def map_change():
-#     if map_type == "speed":
-    
-#         x= "cadence"
-#         st.write("1")
-    
-#     else:
-#         x = "speed"
-#         st.write("2")
-    
-#     return x
 
-# button_go = st.sidebar.button(label="GO")
 
